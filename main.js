5d040dc4796d2704e5ae1d24eab91815040bd2ad
codemantra10 <73803525+codemantra10@users.noreply.github.com>
2021-07-28 11:13:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 function setup(){
-canvas=createCanvas(500,500)
-video=createCapture(VIDEO)
+canvas=createCanvas(500,500);
+video=createCapture(VIDEO);
+//var posenet on line 5 will initailize the model.
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on("pose",GotPoses);
+//"pose"=get alll 17 main body parts.
 }
-
-
 function draw(){
-
+    // The background function makes the canvas get a background.
+background("#00ffe5");
+//The function fill in this case gives you the color of the square
+fill("#00ff00");
+square(30, 20, 55);
 }
 function preload(){
 
+}
+function modelLoaded(){
+console.log("Haha you model is working but sorry to do this though:hahadfjrjgktgigkgfhkjbjnhsasjdbfjdnffmmfthrthtrhrtht.");
+}
+function GotPoses(result){
+if(result.length>0){
+console.log(result);
+}
 }
